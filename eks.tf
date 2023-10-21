b6ae2ad7c52f4fc6a93c8b4905b81b53b9bb320c
Gabriel Maximiniano <gabmaxsan@gmail.com>
2023-10-21 12:12:40 -0300

    first commit

---
+++ eks.tf
@@ -0,0 +1,14 @@
+resource "aws_eks_cluster" "tech-challenge" {
+  name            = "${var.cluster-name}"
+  role_arn        = "${aws_iam_role.tech-challenge-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.tech-challenge-cluster.id}"]
+    subnet_ids         = ["${aws_subnet.tech-challenge.*.id}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.tech-challenge-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.tech-challenge-cluster-AmazonEKSServicePolicy",
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "cluster-name" {
+  default = "eks-tech-challenge"
+  type    = "string"
+}
