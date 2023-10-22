863f487e7ef24b134ddf9b5b4b9b074349dee463
Gabriel Maximiniano <gabmaxsan@gmail.com>
2023-10-22 12:01:21 -0300

    fix error on eks file

--- eks.tf
+++ eks.tf
@@ -3,8 +3,8 @@ resource "aws_eks_cluster" "tech-challenge" {
   role_arn        = "${aws_iam_role.tech-challenge-cluster.arn}"
 
   vpc_config {
-    security_group_ids = ["${aws_security_group.tech-challenge-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.tech-challenge.*.id}"]
+    security_group_ids = [aws_security_group.tech-challenge-cluster.id]
+    subnet_ids         = aws_subnet.tech-challenge[*].id
   }
 
   depends_on = [
