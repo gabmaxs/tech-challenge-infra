9b665471b439eced752f918d88b2d531c58e0089
Gabriel Maximiniano <gabmaxsan@gmail.com>
2023-10-22 15:13:57 -0300

    add: cluster namespace

--- eks.tf
+++ eks.tf
@@ -13,6 +13,16 @@ resource "aws_eks_cluster" "tech-challenge" {
   ]
 }
 
+data "aws_eks_cluster_auth" "tech-challenge" {
+  name = var.cluster-name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.tech-challenge.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.tech-challenge.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.tech-challenge.token
+}
+
 resource "kubernetes_namespace" "tech_challenge" {
   metadata {
     name = "tech-challenge"
