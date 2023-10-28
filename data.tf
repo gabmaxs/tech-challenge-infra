b37f54898aa03b6027820d66277bfd6deda9cb36
Gabriel Maximiniano <gabmaxsan@gmail.com>
2023-10-28 07:16:09 -0300

    try 2

--- data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_eks_cluster_auth" "tech-challenge" {
-  name = aws_eks_cluster.tech-challenge.name
-}
---
+++ terraform.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+
+  required_version = "~> 1.3"
+
+  cloud {
+    organization = "gabmaxs"
+
+    workspaces {
+      name = "tech-challenge"
+    }
+  }
+}
